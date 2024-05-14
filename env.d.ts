c73d74f7201380b13b2d4ea4d49ec3cc65fa6392
Marina_37 <Marina_37@163.com>
2024-05-14 11:27:42 +0800

    代码同步

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+//避免.ts文件识别不了.vue文件
+declare module "*.vue" {
+    import Vue from "vue";
+    export default Vue;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+//引入createApp用于创建应用（花盆）
+import {createApp} from 'vue'
+//引入App根组件（花）
 import App from './App.vue'
 
 createApp(App).mount('#app')
