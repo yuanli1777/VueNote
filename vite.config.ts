eb3960f69791e2635ff3429050a0d4fd687bdd1a
Marina_37 <Marina_37@163.com>
2024-05-15 17:34:39 +0800

    响应式

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueSetupExtend()
   ],
   resolve: {
     alias: {
