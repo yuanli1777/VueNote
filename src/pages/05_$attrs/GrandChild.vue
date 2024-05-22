52f68b7bed013120ce67d01068ff05cf334e4ef1
Marina_37 <Marina_37@163.com>
2024-05-22 18:00:49 +0800

    attrs

--- src/pages/05_$attrs/GrandChild.vue
+++ src/pages/05_$attrs/GrandChild.vue
@@ -12,15 +12,15 @@
 </template>
 
 <script setup lang="ts" name="GrandChild">
-	defineProps(['a','b','c','d','x','y','updateA'])
+defineProps(['a', 'b', 'c', 'd', 'x', 'y', 'updateA'])
 </script>
 
 <style scoped>
-	.grand-child{
-		margin-top: 20px;
-		background-color: orange;
-		padding: 20px;
-		border-radius: 10px;
-		box-shadow: 0 0 10px black;
-	}
+.grand-child {
+	margin-top: 20px;
+	background-color: orange;
+	padding: 20px;
+	border-radius: 10px;
+	box-shadow: 0 0 10px black;
+}
 </style>
